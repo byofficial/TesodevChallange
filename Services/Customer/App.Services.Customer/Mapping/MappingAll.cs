ac0d8c8c216a5596be7eb553b2c1183d53ab0aaf
Burak <burakylddzz@gmail.com>
2021-11-14 13:37:18 +0300

    Mapping İşlemlerinin Yapılması

---
+++ Services/Customer/App.Services.Customer/Mapping/MappingAll.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using App.Services.Customer.DTOs;
+using App.Services.Customer.Models;
+using AutoMapper;
+
+namespace App.Services.Customer.Mapping
+{
+    public class MappingAll:Profile
+    {
+        public MappingAll()
+        {
+            CreateMap<Models.Customer, CustomerDto>().ReverseMap();
+            CreateMap<Address, AddressDto>().ReverseMap();
+
+            CreateMap<Models.Customer, CustomerCreateDto>().ReverseMap();
+            CreateMap<Models.Customer, CustomerUpdateDto>().ReverseMap();
+        }
+    }
+}
