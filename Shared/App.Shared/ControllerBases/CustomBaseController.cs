eebd63304176dd03d7b54a6ac857dbc8829815dc
Burak <burakylddzz@gmail.com>
2021-11-14 16:53:56 +0300

    Shared Sınıf Kütüphanesinde Base Controller Yazılması

---
+++ Shared/App.Shared/ControllerBases/CustomBaseController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using App.Shared.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.Shared.ControllerBases
+{
+    public class CustomBaseController:ControllerBase
+    {
+        public IActionResult CreateActionResultInstance<T>(Response<T> response)
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            };
+        }
+    }
+}
