3d77e90c50e7f188aad84462e909c8cc101451f5
Burak <burakylddzz@gmail.com>
2021-11-14 20:55:25 +0300

    Order Microservice Database Ayarları

---
+++ Services/Order/App.Services.Order/Settings/DatabaseSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Services.Order.Settings
+{
+    public class DatabaseSettings:IDatabaseSettings
+    {
+        public string OrderCollectionName { get; set; }
+        public string ProductCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
