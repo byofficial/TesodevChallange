82494e16bc75fcc2424096e905edd9e46b0146d7
Burak <burakylddzz@gmail.com>
2021-11-14 13:21:39 +0300

    Customer Modellerinin Oluşturulması

---
+++ Services/Customer/App.Services.Customer/Models/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace App.Services.Customer.Models
+{
+    public class Customer
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public Address Address { get; set; }
+
+        [BsonRepresentation(BsonType.DateTime)]
+        public DateTime CreatedAt { get; set; }
+
+        [BsonRepresentation(BsonType.DateTime)]
+        public DateTime UpdatedAt { get; set; }
+    }
+}
