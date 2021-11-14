26650e675d7e89e7bd0d1d72d0fa1e8bb6570b9b
Burak <burakylddzz@gmail.com>
2021-11-14 16:57:58 +0300

    ICustomerInterface Servisinin Oluşturulması

---
+++ Services/Customer/App.Services.Customer/Services/ICustomerService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using App.Services.Customer.DTOs;
+using App.Shared.DTOs;
+
+namespace App.Services.Customer.Services
+{
+    public interface ICustomerService
+    {
+        Task<Response<List<CustomerDto>>> GetAllAsync();
+        Task<Response<CustomerDto>> GetByIdAsync(string id);
+        Task<Response<CustomerDto>> CreateAsync(CustomerCreateDto customerCreateDto);
+        Task<Response<NoContent>> UpdateAsync(CustomerUpdateDto customerUpdateDto);
+        Task<Response<NoContent>> DeleteAsync(string id);
+    }
+}
