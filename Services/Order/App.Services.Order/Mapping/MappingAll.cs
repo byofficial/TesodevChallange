c541826786cd3cf916229019d718d33d616d884e
Burak <burakylddzz@gmail.com>
2021-11-14 20:52:19 +0300

    Order Microservice Mapping İşlemleri

---
+++ Services/Order/App.Services.Order/Mapping/MappingAll.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using App.Services.Order.DTOs;
+using App.Services.Order.Models;
+using AutoMapper;
+
+namespace App.Services.Order.Mapping
+{
+    public class MappingAll:Profile
+    {
+        public MappingAll()
+        {
+            CreateMap<Models.Order, OrderDto>().ReverseMap();
+            CreateMap<Address, AddressDto>().ReverseMap();
+            CreateMap<Product, ProductDto>().ReverseMap();
+
+            CreateMap<Models.Order, OrderCreateDto>().ReverseMap();
+            CreateMap<Models.Order, OrderUpdateDto>().ReverseMap();
+        }
+    }
+}
