712ce5a1fa0b9961a92b8607a96dbaa213aa1589
Burak <burakylddzz@gmail.com>
2021-11-14 20:43:24 +0300

    Order Microservice Model Sınıflarının Oluşturulması

---
+++ Services/Order/App.Services.Order/Models/Order.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace App.Services.Order.Models
+{
+    public class Order
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string CustomerId { get; set; }
+        public int Quantity { get; set; }
+
+        [BsonRepresentation(BsonType.Double)]
+        public double Price { get; set; }
+        public Address Address { get; set; }
+        public Product Product { get; set; }
+
+        [BsonRepresentation(BsonType.DateTime)]
+        public DateTime CreatedAt { get; set; }
+
+        [BsonRepresentation(BsonType.DateTime)]
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ Services/Order/App.Services.Order/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace App.Services.Order.Models
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string ImageUrl { get; set; }
+        public string Name { get; set; }
+    }
+}
