a810bd75ba7d41d250a29447e30583fa9f960171
Burak <burakylddzz@gmail.com>
2021-11-14 21:00:20 +0300

    IOrderService Oluşturulması

---
+++ Services/Order/App.Services.Order/Services/IOrderService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using App.Services.Order.DTOs;
+using App.Shared.DTOs;
+
+namespace App.Services.Order.Services
+{
+    public interface IOrderService
+    {
+        Task<Response<List<OrderDto>>> GetAllAsync();
+        Task<Response<OrderDto>> GetByIdAsync(string id);
+        Task<Response<List<OrderDto>>> GetAllByUserIdAsync(string userId);
+        Task<Response<OrderDto>> CreateAsync(OrderCreateDto orderCreateDto);
+        Task<Response<NoContent>> UpdateAsync(OrderUpdateDto orderUpdateDto);
+        Task<Response<NoContent>> DeleteAsync(string id);
+    }
+}
